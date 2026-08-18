522b095f1bba2a11867fb8aaf48e66dd14cd0d75
Dezl00 <Dezl00@users.noreply.github.com>
2026-08-18 03:59:19 +0300

    fix: include sparticuz bin files in Vercel Node File Trace

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+  outputFileTracingIncludes: {
+    '/api/generate-pdf': ['./node_modules/@sparticuz/chromium/**/*'],
+  },
 };
 
 module.exports = nextConfig;
